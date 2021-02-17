0603f6d8e77ee17448e5cbf8a16766b02007c890
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-02-17 16:15:04 +0900

    Eigen導入

---
+++ CG_method/a1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    Matrix3d M;
+
+    M << 1.1, 2.2, 3.3,
+         4.4, 5.5, 6.6,
+         7.7, 8.8, 9.9;
+
+    return 0;
+}
