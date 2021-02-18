2ee68b36860bba789fe07bd217f2816905236d64
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-02-18 12:23:37 +0900

    コメント追加

--- CG_method/a2.cpp
+++ CG_method/a2.cpp
@@ -8,16 +8,15 @@ using namespace std;
 #define TMAX 100
 #define EPS 1.0e-8      // 許される誤差の上限
 
+// CG法：共役勾配法
 VectorXd CG_method(MatrixXd A, VectorXd b, VectorXd x) {
     VectorXd p(N), r(N), Ax(N), Ap(N);
 
     // Axを計算
     Ax = A * x;
-
     // pとrを計算 p = r := b - Ax
     p = b - Ax; // p:初期勾配ベクトル
     r = p;      // 初期残差ベクトル
-
     // 反復計算
     for (int i = 0; i < TMAX; i++)
     {
@@ -26,10 +25,10 @@ VectorXd CG_method(MatrixXd A, VectorXd b, VectorXd x) {
         // alphaを計算
         Ap = A * p;
         alpha = p.dot(r) / p.dot(Ap);
-
+        // x, rを更新
         x += alpha * p;
         r -= alpha * Ap;
-
+        // 誤差を計算
         error = r.norm();
         printf("LOOP : %d\t Error : %g\n", i, error);
 
@@ -67,10 +66,9 @@ int main(int argc, char const *argv[])
     b << 3.0, 1.0, 4.0, 0.0, 5.0, -1.0, 6.0, -2.0, 7.0, -15.0;
     x << 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0;
 
-    cout << x << endl;
-
+    // CG法
     x = CG_method(A, b, x);
-
+    // 出力
     cout << x << endl;
 
     return 0;
